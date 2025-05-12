4083c417b410623ed1172aa02ccf3db766ba7093
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-05-12 12:18:33 +1000

    refactor: Pass parameter by reference in `build_{table_of_contents,command}_markdown()`
    We can just take a reference to `parent_command_path` without consuming it and create an
    owned copy inside the function instead.

--- src/lib.rs
+++ src/lib.rs
@@ -169,8 +169,7 @@ fn write_help_markdown(
     if options.show_table_of_contents {
         writeln!(buffer, "**Command Overview:**\n").unwrap();
 
-        build_table_of_contents_markdown(buffer, Vec::new(), command, 0)
-            .unwrap();
+        build_table_of_contents_markdown(buffer, &[], command, 0).unwrap();
 
         writeln!(buffer).unwrap();
     }
@@ -179,7 +178,7 @@ fn write_help_markdown(
     // Write the commands/subcommands sections
     //----------------------------------------
 
-    build_command_markdown(buffer, Vec::new(), command, 0, options).unwrap();
+    build_command_markdown(buffer, &[], command, 0, options).unwrap();
 
     //-----------------
     // Write the footer
@@ -198,7 +197,7 @@ fn write_help_markdown(
 fn build_table_of_contents_markdown(
     buffer: &mut String,
     // Parent commands of `command`.
-    parent_command_path: Vec<String>,
+    parent_command_path: &[String],
     command: &clap::Command,
     depth: usize,
 ) -> std::fmt::Result {
@@ -212,7 +211,7 @@ fn build_table_of_contents_markdown(
 
     // Append the name of `command` to `command_path`.
     let command_path = {
-        let mut command_path = parent_command_path;
+        let mut command_path = parent_command_path.to_owned();
         command_path.push(title_name);
         command_path
     };
@@ -231,7 +230,7 @@ fn build_table_of_contents_markdown(
     for subcommand in command.get_subcommands() {
         build_table_of_contents_markdown(
             buffer,
-            command_path.clone(),
+            &command_path,
             subcommand,
             depth + 1,
         )?;
@@ -288,7 +287,7 @@ fn build_table_of_contents_html(
 fn build_command_markdown(
     buffer: &mut String,
     // Parent commands of `command`.
-    parent_command_path: Vec<String>,
+    parent_command_path: &[String],
     command: &clap::Command,
     depth: usize,
     options: &MarkdownOptions,
@@ -303,7 +302,7 @@ fn build_command_markdown(
 
     // Append the name of `command` to `command_path`.
     let command_path = {
-        let mut command_path = parent_command_path.clone();
+        let mut command_path = parent_command_path.to_owned();
         command_path.push(title_name);
         command_path
     };
@@ -438,7 +437,7 @@ fn build_command_markdown(
     for subcommand in command.get_subcommands() {
         build_command_markdown(
             buffer,
-            command_path.clone(),
+            &command_path,
             subcommand,
             depth + 1,
             options,
